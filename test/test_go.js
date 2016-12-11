07285c0f8639c55888a9f6a5f3b71047761e315b
Matheus Portela <matheus.v.portela@gmail.com>
2016-12-11 17:38:45 -0200

    Commit server with / route

---
+++ test/test_go.js
@@ -0,0 +1,74 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var http = require('http');
+var go = require('../lib/go');
+
+var assert = chai.assert;
+
+chai.use(chaiHttp);
+
+describe('go.js', function() {
+    var server = undefined;
+
+    before(function() {
+        server = go.server.listen(8080);
+    });
+
+    after(function() {
+        server.close();
+    });
+
+    describe('/', function() {
+        it('expect server reply with status "ok"', (done) => {
+            chai.request(server)
+                .get('/')
+                .end((err, res) => {
+                    var data = JSON.stringify(res.body);
+                    var expect = JSON.stringify({'status': 'ok'});
+                    assert.equal(res.status, 200);
+                    assert.equal(data, expect);
+                    done();
+                });
+        });
+    });
+
+    describe('/games', function() {
+        it('expect empty list of games', (done) => {
+            chai.request(server)
+                .get('/games')
+                .end((err, res) => {
+                    var data = JSON.stringify(res.body);
+                    var expect = JSON.stringify({'games': []});
+                    assert.equal(res.status, 200);
+                    assert.equal(data, expect);
+                    done();
+                })
+        });
+
+        it('expect new game in list of games', (done) => {
+            chai.request(server)
+                .post('/games')
+                .send({'game-owner': 'user1', 'game-challenger': 'user2'})
+                .end((err, res) => {
+                    done();
+                });
+
+            chai.request(server)
+                .get('/games')
+                .end((err, res) => {
+                    var data = JSON.stringify(res.body);
+                    var expect = JSON.stringify({
+                        'games': [
+                            {
+                                'game-owner': 'user1',
+                                'game-challenger': 'user2'
+                            }
+                        ]
+                    });
+                    assert.equal(res.status, 200);
+                    assert.equal(data, expect);
+                    done();
+                });
+        });
+    });
+});
